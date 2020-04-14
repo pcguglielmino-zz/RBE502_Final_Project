576a89048e053810b9a828dffc24d32e0c14784a
Mandar Deshmukh <63661479+mandardeshmukh03@users.noreply.github.com>
2020-04-14 13:33:27 -0400

    Update Main.m
    instead of fixing the values of M C G  using the usual standard code.

--- Main.m
+++ Main.m
@@ -1,1 +1,22 @@
 %% Place for the project code
+I1=10;  I2 = 10; m1=5; r1=.5; m2=5; r2=.5; l1=1; l2=1;
+g=9.8;
+
+% we compute the parameters in the dynamic model
+a = I1+I2+m1*r1^2+ m2*(l1^2+ r2^2);
+b = m2*l1*r2;
+d = I2+ m2*r2^2;
+%%
+symx= sym('X',[4,1]); 
+
+M = [a+2*b*cos(symx(2)), d+b*cos(symx(2));
+    d+b*cos(symx(2)), d];
+C = [-b*sin(symx(2))*symx(4), -b*sin(symx(2))*(symx(3)+symx(4)); b*sin(symx(2))*symx(3),0];
+G = [m1*g*r1*cos(symx(1))+m2*g*(l1*cos(symx(1))+r2*cos(symx(1)+symx(2)));
+    m2*g*r2*cos(symx(1)+symx(2))];
+
+X = sym('X',[4,1]);
+M = subs(M, symx, X);
+C = subs(C, symx, X);
+G = subs(G, symx, X);
+invM = inv(M);
